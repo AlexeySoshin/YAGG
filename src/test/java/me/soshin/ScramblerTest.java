3d9a2ee6a0e176c2ac1c341b735f499692d28ef3
Alexey Soshin <alexey.soshin@gettaxi.com>
2017-03-29 18:25:31 +0300

    Add test for Scrambler

---
+++ src/test/java/me/soshin/ScramblerTest.java
@@ -0,0 +1,42 @@
+package me.soshin;
+
+import com.google.gson.JsonElement;
+import me.soshin.fetcher.GitHubFetcher;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class ScramblerTest {
+
+    @Test
+    public void testScramble() throws IOException {
+
+        final Scrambler s = new Scrambler();
+
+        final GitHubFetcher.Events events = s.doYourJob("./src/test/resources/events.json");
+
+        for (final GitHubFetcher.Events.Service service : events.services.values()) {
+
+            for (final String publishEvent : service.publish) {
+                assertEquals(publishEvent.length(), 8);
+            }
+
+            for (final JsonElement subscribeEvent : service.subscribe) {
+                final String eventName;
+                if (subscribeEvent.isJsonPrimitive()) {
+                    eventName = subscribeEvent.getAsString();
+                } else {
+                    eventName = subscribeEvent.getAsJsonObject().get("type").getAsString();
+                }
+
+                assertEquals(String.format("%s should be scrambled", eventName), eventName.length(), 8);
+            }
+        }
+    }
+
+}
